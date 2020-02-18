a99d2ca36ced9a16c30a346a86ac3fcc7ef51516
eileenxu9 <rose007563@gmail.com>
2020-02-18 08:11:48 +0000

    We added our final code from the testing-branch to the master branch. Our final strategy is to betray when the other player betrayed in the last round. If the other player colluded, then we use the probality_that_other_player_will_betray() function.

--- team2.py
+++ team2.py
@@ -7,24 +7,39 @@
 ####
 
 team_name = 'barcelona' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+strategy_name = 'Betray when other player betrayed'
+strategy_description = 'Betray first. Betray when other players last move was betray. Otherwise, compare all rounds to the previous round and assume opponent will behave the same as the first time the previous rounds result occurred. If the previous rounds result never has happened, betray except after being severely punished.'
+
+def probability_that_other_player_will_betray(my_history, their_history, my_score, their_score):
+  their_previous_round = their_history[-1]
+  my_previous_round = my_history[-1]
+  
+# Look at rounds before that one
+  for round in range(len(my_history)-1):
+    their_prior_round = their_history[round]
+    my_prior_round = my_history[round]
+    # If one matches
+    if (my_prior_round == my_previous_round) and (their_prior_round == their_previous_round):
+      return their_history[round]
+        # No match found
+  if my_history[-1]=='c' and their_history[-1]=='b':
+    return 'b' # Betray if we were severely punished last time.
+  else:
+    return 'c' # Otherwise collude.
+
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+  '''Make my move based on the history with this player.
+  
+  history: a string with one letter (c or b) per round that has been played with this opponent.
+  their_history: a string of the same length as history, possibly empty. 
+  The first round between these two players is my_history[0] and their_history[0]
+  The most recent round is my_history[-1] and their_history[-1]
+  
+  Returns 'c' or 'b' for collude or betray.
+  '''
+  if len(my_history)==0: # It's the first round; betray.
+    return 'b'
+  elif their_history[-1]=='b':
+    return 'b' # Betray if their last move was betray.
+  else:
+    probability_that_other_player_will_betray(my_history, their_history, my_score, their_score) # otherwise call probability_that_other_player_will_betray() function
