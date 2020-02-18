7da7ae79f3d324d081f3de29d5d43266faff70b6
eileenxu9 <rose007563@gmail.com>
2020-02-18 07:59:07 +0000

    We created all 3 strategies and made a new function named probability_that_other_player_will_betray() to use in our final algorithm. The 3 strategies we coded were to betray when the other player’s last move was betray, betray if we were severely punished last time, and alternate between betray and collude.

--- example0.py
+++ example0.py
@@ -7,20 +7,40 @@
 ####
 
 team_name = 'E0'
-strategy_name = 'Collude'
-strategy_description = 'Always collude.'
-    
+strategy_name = 'Betray when other player betrayed'
+strategy_description = 'Betray first. Betray when other players last move was betray. Otherwise, compare all rounds to the previous round and assume opponent will behave the same as the first time the previous rounds result occurred. If the previous rounds result never has happened, betray except after being severely punished.'
+
+def probability_that_other_player_will_betray(my_history, their_history, my_score, their_score):
+  their_previous_round = their_history[-1]
+  my_previous_round = my_history[-1]
+  
+# Look at rounds before that one
+  for round in range(len(my_history)-1):
+    their_prior_round = their_history[round]
+    my_prior_round = my_history[round]
+    # If one matches
+    if (my_prior_round == my_previous_round) and (their_prior_round == their_previous_round):
+      return their_history[round]
+        # No match found
+  if my_history[-1]=='c' and their_history[-1]=='b':
+    return 'b' # Betray if we were severely punished last time.
+  else:
+    return 'c' # Otherwise collude.
+
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    # This player always colludes.
-    return 'c'
+  '''Make my move based on the history with this player.
+  
+  history: a string with one letter (c or b) per round that has been played with this opponent.
+  their_history: a string of the same length as history, possibly empty. 
+  The first round between these two players is my_history[0] and their_history[0]
+  The most recent round is my_history[-1] and their_history[-1]
+  
+  Returns 'c' or 'b' for collude or betray.
+  '''
+  if len(my_history)==0: # It's the first round; betray.
+    return 'b'
+  elif their_history[-1]=='b':
+    return 'b' # Betray if their last move was betray.
+  else:
+    probability_that_other_player_will_betray(my_history, their_history, my_score, their_score) # otherwise call probability_that_other_player_will_betray() function
     
--- example1.py
+++ example1.py
@@ -7,19 +7,40 @@
 ####
 
 team_name = 'E1'
-strategy_name = 'Betray'
-strategy_description = 'Always betray.'
-    
-def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
+strategy_name = 'Betray if severely punished last round'
+strategy_description = 'Betray first round. Compare all rounds to the previous round and assume opponent will behave the same as the first time the previous rounds result occurred. If the previous rounds result never has happened, collude except after being severly punished.'
+
+def probability_that_other_player_will_betray(my_history, their_history, my_score, their_score):
+  their_previous_round = their_history[-1]
+  my_previous_round = my_history[-1]
+
+  # Look at rounds before that one
+  if my_score > -100:
+    for round in range(len(my_history)-1):
+      their_prior_round = their_history[round]
+      my_prior_round = my_history[round]
+      # If one matches
+      if (my_prior_round == my_previous_round) and (their_prior_round == their_previous_round):
+        return my_history[round]
+          # No match found
+    if my_history[-1]=='c' and their_history[-1]=='b':
+      return 'b' # Betray if we were severely punished last time
+    else:
+      return 'c' # Otherwise collude.
+  else:
     return 'b'
+
+def move(my_history, their_history, my_score, their_score):
+  '''Make my move based on the history with this player.
+  
+  history: a string with one letter (c or b) per round that has been played with this opponent.
+  their_history: a string of the same length as history, possibly empty. 
+  The first round between these two players is my_history[0] and their_history[0]
+  The most recent round is my_history[-1] and their_history[-1]
+  
+  Returns 'c' or 'b' for collude or betray.
+  '''
+  if len(my_history)==0: # It's the first round; betray.
+    return 'b'
+  probability_that_other_player_will_betray(my_history, their_history, my_score, their_score)
+  
